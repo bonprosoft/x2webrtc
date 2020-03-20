10cf9bb74118448a18eb34e08f6a02f6e72fcce8
Yuki Igarashi <me@bonprosoft.com>
2020-03-21 02:39:51 +0900

    Subscribe canvas click event

--- viewer/src/screen.ts
+++ viewer/src/screen.ts
@@ -16,6 +16,8 @@ export class MediaStreamScreen {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
 
+        this.canvas.addEventListener("click", this.canvasClicked);
+
         // NOTE(igarashi): create a video element to render stream
         this.video = document.createElement("video");
         this.video.onresize = (e) => {
@@ -27,6 +29,17 @@ export class MediaStreamScreen {
             this.updateCanvasSize();
             this.updateFrameByVideo();
         }
+    }
+
+    private canvasClicked = (e: MouseEvent) => {
+        const rect = (<HTMLElement>e.target).getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const screenX = (x / rect.width) * this.canvas.width;
+        const screenY = (y / rect.height) * this.canvas.height;
+
+        console.log(`${screenX}, ${screenY}`);
     }
 
     private updateFrameByVideo = () => {
