4754a72b3bf5dc11a479f60fcffda0c09fa3c333
Yuki Igarashi <me@bonprosoft.com>
2020-03-21 01:07:31 +0900

    Specify install_requires

--- client/setup.py
+++ client/setup.py
@@ -6,4 +6,11 @@ setup(
     name="momo",
     version="0.0.0",
     packages=find_packages(),
+    install_requires=[
+        "Pillow>=7.0.0,<7.1.0",
+        "aiortc>=0.9.0,<0.10.0",
+        "av>=7.0.0,<8.0.0",
+        "numpy>=1.18.0,<1.19.0",
+        "python-xlib>=0.26,<1.0",
+    ],
 )
