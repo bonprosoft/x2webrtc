b542526d91f9f8fa7be730f437cc97fa8d0cd420
Yuki Igarashi <me@bonprosoft.com>
2020-03-21 18:01:30 +0900

    Implement corresponding model classes in viewer

---
+++ viewer/src/models.ts
@@ -0,0 +1,30 @@
+
+export class MouseMoveEvent {
+    kind: "mouse-move";
+    x: number;
+    y: number;
+}
+
+export enum MouseButtonKind {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+}
+
+export enum ButtonEventKind {
+    ButtonDown = 0,
+    ButtonUp = 1,
+}
+
+export class MouseButtonEvent {
+    kind: "mouse-button";
+    button_kind: MouseButtonKind;
+    event_kind: ButtonEventKind;
+}
+
+export type ScreenEvents = MouseMoveEvent | MouseButtonEvent;
+
+export class EventReports {
+    kind: "event";
+    events: ScreenEvents[];
+}
