f0d0333383badaebfa9b3f647d5275274a96e3a1
Yuki Igarashi <me@bonprosoft.com>
2020-03-21 18:00:43 +0900

    Update test

--- client/tests/test_models.py
+++ client/tests/test_models.py
@@ -1,15 +1,21 @@
 import json
 
-from x2webrtc.models import (ButtonEventKind, MouseButtonEvent,
-                             MouseButtonKind, MouseMoveEvent, ScreenEvents,
-                             from_dict, to_dict)
+from x2webrtc.models import (
+    ButtonEventKind,
+    EventReport,
+    MouseButtonEvent,
+    MouseButtonKind,
+    MouseMoveEvent,
+    from_dict,
+    to_dict,
+)
 
 
 def test_convert_convertback():
     a = MouseMoveEvent(10, 20)
     b = MouseButtonEvent(MouseButtonKind.LEFT_BUTTON, ButtonEventKind.BUTTON_DOWN)
     c = MouseButtonEvent(MouseButtonKind.LEFT_BUTTON, ButtonEventKind.BUTTON_UP)
-    data = ScreenEvents([a, b, c])
+    data = EventReport([a, b, c])
     serialized = json.dumps(to_dict(data))
 
     deserialized = from_dict(json.loads(serialized))
